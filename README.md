afaed5baf9178685ff81b8c95b30dfc8fe3608f5
fsodari <franksodari@gmail.com>
2021-06-01 19:52:50 -0700

    ...

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # PineScope
+
+## Cross-Compiling
+My build computer: Ubuntu
+My pine phone: Manjaro, plasma mobile
+
+### Sync pinephone sysroot to build system using rsync
+rsync -avz user@ip:/usr user@ip:/lib /home/user/pine-sysroot
+
+### Install toolchain on build system
+sudo apt install gcc-aarch64-linux-gnu
---
+++ pine.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
+
+set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
+
+set(CMAKE_SYSROOT /home/frank/pine-sysroot)
+set(CMAKE_STAGING_PREFIX /home/frank/pine-devel/stage)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
